3a9fdfb4b8f04c171a843d3fb61e686075cae9ef
Surya Pratap <surps175@gmail.com>
2021-02-05 14:41:34 +0530

    Initial commit for first app

---
+++ app/src/main/java/com/suryap/application1/MainActivity.java
@@ -0,0 +1,68 @@
+package com.suryap.application1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText et1,et2;
+    Button add,sub,mul,div;
+    TextView res;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        et1=findViewById(R.id.et1);
+        et2=findViewById(R.id.et2);
+        add=findViewById(R.id.add);
+        sub=findViewById(R.id.sub);
+        mul=findViewById(R.id.mul);
+        div=findViewById(R.id.div);
+        res=findViewById(R.id.res);
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int f,s,a;
+                f=Integer.parseInt(et1.getText().toString());
+                s=Integer.parseInt(et2.getText().toString());
+                a=f+s;
+                res.setText("Result=" +a);
+            }
+        });
+        sub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int f,s,a;
+                f=Integer.parseInt(et1.getText().toString());
+                s=Integer.parseInt(et2.getText().toString());
+                a=f-s;
+                res.setText("Result=" +a);
+            }
+        });
+        mul.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int f,s,a;
+                f=Integer.parseInt(et1.getText().toString());
+                s=Integer.parseInt(et2.getText().toString());
+                a=f*s;
+                res.setText("Result=" +a);
+            }
+        });
+        div.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                float f,s,a;
+                f=Integer.parseInt(et1.getText().toString());
+                s=Integer.parseInt(et2.getText().toString());
+                a=f/s;
+                res.setText("Result=" +a);
+            }
+        });
+    }
+}
+
